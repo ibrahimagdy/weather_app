2301573f2d0af59c3e429cfe2bdcfa2e900fc284
Ibrahim Magdy <iibbrraahhimm00@gmail.com>
2024-06-01 05:12:10 +0300

    start app

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/views/home_view.dart';
+
+void main() {
+  runApp(const WeatherApp());
+}
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeView(),
+    );
+  }
+}
