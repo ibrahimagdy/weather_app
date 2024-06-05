4620be1b66b00a6dc21ecaa1d2cbbde84c65ac70
Ibrahim Magdy <iibbrraahhimm00@gmail.com>
2024-06-05 06:31:05 +0300

    add fromJson weather model

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -15,4 +15,17 @@ class WeatherModel {
       required this.maxTemp,
       required this.minTemp,
       required this.weatherCondition});
+
+  factory WeatherModel.fromJson(json) {
+    return WeatherModel(
+      city: json["location"]["name"],
+      lastUpdated: json["current"]["last_updated"],
+      image: json["forecast"]["forecastday"][0]["day"]["condition"]["icon"],
+      temp: json["forecast"]["forecastday"][0]["day"]["avgtemp_c"],
+      maxTemp: json["forecast"]["forecastday"][0]["day"]["maxtemp_c"],
+      minTemp: json["forecast"]["forecastday"][0]["day"]["mintemp_c"],
+      weatherCondition: json["forecast"]["forecastday"][0]["day"]["condition"]
+          ["text"],
+    );
+  }
 }
